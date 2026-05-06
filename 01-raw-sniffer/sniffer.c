930553435eecef11f4da5c21d77e26f8c86c5c51
katswcode <sebasherranp@gmail.com>
2026-05-05 19:04:03 -0500

    PCAP file added and function created to view the name of the ports in terminal

--- 01-raw-sniffer/sniffer.c
+++ 01-raw-sniffer/sniffer.c
@@ -7,6 +7,52 @@
 #include <linux/icmp.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
+#include <stdint.h>
+#include <sys/time.h>
+
+
+// Global Header Structure of PCAP
+
+typedef struct pcap_hdr_s 
+
+{
+    uint32_t magic_number;   /* magic number */
+    uint16_t version_major;  /* major version number */
+    uint16_t version_minor;  /* minor version number */
+    int32_t  thiszone;       /* GMT to local correction */
+    uint32_t sigfigs;        /* accuracy of timestamps */
+    uint32_t snaplen;        /* max length of captured packets, in octets */
+    uint32_t network;        /* data link type (1 = Ethernet) */
+} pcap_hdr_t;
+
+// Packet Header Structure of PCAP
+
+typedef struct pcaprec_hdr_s 
+
+{
+    uint32_t ts_sec;         /* timestamp seconds */
+    uint32_t ts_usec;        /* timestamp microseconds */
+    uint32_t incl_len;       /* number of octets of packet saved in file */
+    uint32_t orig_len;       /* actual length of packet */
+} pcaprec_hdr_t;
+
+// Function to get the name of the port based on the port number
+
+const char* get_port_name(uint16_t port) 
+
+{
+    switch (port) {
+        case 80:   return "HTTP";
+        case 443:  return "HTTPS (TLS)";
+        case 53:   return "DNS";
+        case 22:   return "SSH";
+        case 21:   return "FTP";
+        case 123:  return "NTP";
+        default:   return "Unknown";
+    }
+}
+
+
 // start main function to create the sniffer
 
 int main(int argc, char const *argv[])
@@ -66,6 +112,27 @@ int main(int argc, char const *argv[])
 
     // Creating while to repeatly capture data    
 
+    // open file in write binary mode to save the captured packets in a pcap file
+
+    FILE *logfile = fopen("capture.pcap", "wb");
+    if(logfile == NULL) {
+        printf("Error al abrir el archivo PCAP.\n");
+        return 1;
+    }
+
+    // fill and write the global header of the pcap file
+
+    pcap_hdr_t global_hdr;
+    global_hdr.magic_number = 0xa1b2c3d4; // Wireshark search magic number to identify the file as a pcap file
+    global_hdr.version_major = 2;
+    global_hdr.version_minor = 4;
+    global_hdr.thiszone = 0;
+    global_hdr.sigfigs = 0;
+    global_hdr.snaplen = 65535; // max length of captured packets
+    global_hdr.network = 1;     // Refers to layer 2 (Ethernet)
+    fwrite(&global_hdr, sizeof(pcap_hdr_t), 1, logfile);
+    fflush(logfile); // forze writing the header to the file
+
     while(1) 
     
     {
@@ -78,6 +145,25 @@ int main(int argc, char const *argv[])
     // Creting the reciving from function to recive data from the raw socket and collecting them in the buffer
 
     int data_size = recvfrom(raw_socket, buffer, 65536, 0, NULL, NULL);
+
+    // 1. get the current timestamp to fill the packet header of the pcap file (we use gettimeofday to get the time in seconds and microseconds)
+
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+
+        // 2. fill the packet header
+
+        pcaprec_hdr_t pkt_hdr;
+        pkt_hdr.ts_sec = tv.tv_sec;
+        pkt_hdr.ts_usec = tv.tv_usec;
+        pkt_hdr.incl_len = data_size; // captured size of the packet (could be smaller than orig_len if snaplen is smaller)
+        pkt_hdr.orig_len = data_size; // original size of the packet (could be larger than incl_len if snaplen is smaller)
+
+        // 3. write the packet header and raw data to the pcap file
+
+        fwrite(&pkt_hdr, sizeof(pcaprec_hdr_t), 1, logfile);
+        fwrite(buffer, data_size, 1, logfile);
+        fflush(logfile); // save in real time the captured packet in the pcap file
 
         // If the data size is less than 0 there is an error if not print the length of the packet
 
@@ -185,7 +271,7 @@ int main(int argc, char const *argv[])
 
                 struct tcphdr *tcp = (struct tcphdr *)(buffer + sizeof(struct ethhdr) + (ip->ihl * 4));
 
-                printf("Protocol: TCP | Origin port: %d | Destiny port: %d \n",ntohs(tcp->source), ntohs(tcp->dest));
+                printf("TCP Port: %u (%s)\n", ntohs(tcp->source), get_port_name(ntohs(tcp->source)));
         
                 }
 
@@ -197,7 +283,7 @@ int main(int argc, char const *argv[])
 
                 struct udphdr *udp = (struct udphdr*)(buffer + sizeof(struct ethhdr) + (ip->ihl *4));
 
-                printf("Protocol UDP | Origin port: %d | Destiny port: %d \n", ntohs(udp->source), ntohs(udp->dest)); 
+                printf("UDP Port: %u (%s)\n", ntohs(udp->source), get_port_name(ntohs(udp->source))); 
 
                 }
         
